1429eaf5dde3ec13ece3da67df1e07df0a86a6ed
mitchellscot <Mitchellscott@me.com>
2021-01-29 13:51:25 -0600

    finished styling

--- src/components/AddItemForm/AddItemForm.jsx
+++ src/components/AddItemForm/AddItemForm.jsx
@@ -4,13 +4,16 @@ function addItemForm(props) {
         return (
             <form onSubmit={props.addItem}>
                 <div class="form-text" class="mb-3">
-                <label class="form-label" htmlFor="itemInput">Item: </label>
+                <div class="input-group mb-3">
+                <span class="input-group-text"htmlFor="itemNameInput">Item:</span>
                 <input class="form-control" id="itemNameInput" value={props.newItemName} onChange={(event) => { props.setNewItemName(event.target.value) }}
-                    required></input>
-                <label class="form-label" htmlFor="quantityInput">Quantity: </label>
+                    required></input></div>
+                <div class="input-group mb-3">
+                <span class="input-group-text">Quantity</span>
                 <input class="form-control" id="quantityInput" value={props.newItemQuantity} onChange={(event) => { props.setNewItemQuantity(event.target.value) }} required></input>
-                <label class="form-label" htmlFor="unitInput">Unit: </label>
+                <span class="input-group-text">Unit</span>
                 <input class="form-control" id="unitInput" value={props.newItemUnit} onChange={(event) => { props.setNewItemUnit(event.target.value) }} required></input>
+                </div>
                 <button class="btn btn-primary" type="submit">Save</button>
                 </div>
             </form>
@@ -20,13 +23,15 @@ function addItemForm(props) {
         return (
         <form onSubmit={props.updateItem}>
             <div class="form-text" class="mb-3">
-            <label class="form-label" htmlFor="itemInput">Item: </label>
+            <div class="input-group mb-3">
+            <span class="input-group-text">Item:</span>
             <input class="form-control" id="itemNameInput" value={props.newItemName} onChange={(event) => { props.setNewItemName(event.target.value) }}
-                required></input>
-            <label class="form-label" htmlFor="quantityInput">Quantity: </label>
+                required></input></div>
+            <div class="input-group mb-3">
+            <span class="input-group-text">Quantity</span>
             <input class="form-control" id="quantityInput" value={props.newItemQuantity} onChange={(event) => { props.setNewItemQuantity(event.target.value) }} required></input>
-            <label class="form-label"htmlFor="unitInput">Unit: </label>
-            <input class="form-control"  id="unitInput" value={props.newItemUnit} onChange={(event) => { props.setNewItemUnit(event.target.value) }} required></input>
+            <span class="input-group-text">Unit</span>
+            <input class="form-control"  id="unitInput" value={props.newItemUnit} onChange={(event) => { props.setNewItemUnit(event.target.value) }} required></input></div>
             <button class="btn btn-primary" type="submit">Save</button></div>
         </form>
         )
@@ -37,3 +42,8 @@ function addItemForm(props) {
 export default addItemForm;
 
 //TODO addItem pass in as props
+/*<div class="input-group mb-3">
+  <input type="text" class="form-control" placeholder="Username" aria-label="Username">
+  <span class="input-group-text">@</span>
+  <input type="text" class="form-control" placeholder="Server" aria-label="Server">
+</div>*/
